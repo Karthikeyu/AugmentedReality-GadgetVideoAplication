43199953148a037a308a46f07dfc0742875fd4b1
Busa karthikeyu <busa@Busas-MacBook-Air.local>
2019-11-29 01:07:08 +0100

    Adding augmentable gadget image application

---
+++ Assets/GadgetVideo.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class GadgetVideo : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject Mac, Case, Camera;
+    VirtualButtonBehaviour[] virtualButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         virtualButton = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int v = 0; v < virtualButton.Length; v++)
+        {
+            virtualButton[v].RegisterEventHandler(this);
+        }
+
+        Camera.SetActive(false);
+        Case.SetActive(false);
+        Mac.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        switch (vb.VirtualButtonName)
+        {
+            case "MacButton":
+                Mac.SetActive(true);
+                Camera.SetActive(false);
+                Case.SetActive(false);
+                Debug.Log("MacButton");
+                break;
+            case "CaseButton":
+                Mac.SetActive(false);
+                Camera.SetActive(false);
+                Case.SetActive(true);
+                Debug.Log("CaseButton");
+                break;
+            case "CameraButton":
+                Mac.SetActive(false);
+                Camera.SetActive(true);
+                Case.SetActive(false);
+                Debug.Log("CameraButton");
+                break;
+            default:
+                throw new UnityException("Button not found!");
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # AugmentedReality-GadgetVideoAplication
+
+This is a initial level user interaction augmented reality android application. This is developed using Unity, Vuforia with android SDK. When mobile camera opens after installing, camera looks for the image of the gadgets. As soon as image is found, when user clicks on the gadget in the image corresponding videos opens up and play.
+
+The application could not be uploaded to git repository as the file size is greater than 100 mb. Click on the below link to download apk.
+
+https://drive.google.com/open?id=1VTZlysagGYVzU7FXOZd9UuuS4j1mKbSb
+
+
+Sources of videos are:
+
+i. https://www.youtube.com/watch?v=xdhEXr1zw6s
+
+ii. https://www.youtube.com/watch?v=1yVF-N__JKk
+
+iii. https://www.youtube.com/watch?v=VvoIVCnuBLw
